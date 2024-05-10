3a1d9718b7291e4268474e82d66fa0c8b99a7946
Veronica <veronica.celemin1@vodafone.com>
2024-05-10 12:19:42 +0200

    feat: add tests to reach 100% mutation coverage

--- src/utils/isFrozenWater.test.ts
+++ src/utils/isFrozenWater.test.ts
@@ -4,3 +4,13 @@ test("should return true when the temperature of the water is -15ºC", () => {
   const isIce = isFrozenWater(-15);
   expect(isIce).toBe(true);
 });
+
+test("should return true when the temperature of the water is 0ºC", () => {
+  const isIce = isFrozenWater(0);
+  expect(isIce).toBe(true);
+});
+
+test("should return false when the temperature of the water is 15ºC", () => {
+  const isIce = isFrozenWater(15);
+  expect(isIce).toBe(false);
+});
